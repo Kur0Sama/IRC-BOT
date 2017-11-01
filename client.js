48ebebeffe67266370cc6b443e39757bb382f810
Kur0Sama <32812112+Kur0Sama@users.noreply.github.com>
2017-11-01 20:18:57 +0100

    UPDATED

--- client.js
+++ client.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 
 let channels = JSON.parse(fs.readFileSync('channels.json', 'utf8'));
 
-const TARGET_MINUTE = 15; // Minute of the hour when the chest will refresh, 30 means 1:30, 2:30, etc.
+const TARGET_MINUTE = 20; // Minute of the hour when the chest will refresh, 30 means 1:30, 2:30, etc.
 const OFFSET = 0; // Notification will be sent this many minutes before the target time, must be an integer
 const NOTIFY_MINUTE = (TARGET_MINUTE < OFFSET ? 60 : 0) + TARGET_MINUTE - OFFSET;
 
@@ -14,13 +14,11 @@ setInterval(function () {
     channels['IRCs'].forEach(function (i) {
         var d = new Date();
         if (d.getMinutes() !== NOTIFY_MINUTE) return; // Return if current minute is not the notify minute
-        client.channels.get(i).send({
-            embed: {
-                description: `\`\`\`asciidoc\n= COMMENT M'INVITER =\nLien : https://discordapp.com/oauth2/authorize?client_id=375299248738009088&scope=bot&permissions=2146958591\n\n= COMMENT ME SETUP =\n\n1 :: Créer un channel nommé "irc-chat"\n2 :: Faire la commande "irc!sync"\n3 :: Allez dans le channel et testez !\`\`\``,
-                thumbnail: client.user.avatarURL,
-                color: 0x3f4bff
-            }
-        });
+        let embed = new Discord.RichEmbed();
+        embed.setColor(0x3f4bff);
+        embed.setDescription(`\`\`\`asciidoc\n= COMMENT M'INVITER =\nLien : https://discordapp.com/oauth2/authorize?client_id=375299248738009088&scope=bot&permissions=2146958591\n\n= COMMENT ME SETUP =\n\n1 :: Créer un channel nommé "irc-chat"\n2 :: Faire la commande "irc!sync"\n3 :: Allez dans le channel et testez !\`\`\``);
+        embed.setThumbnail(client.user.avatarURL);
+        client.channels.get(i).send(embed);
     });
 }, 60 * 1000); // Check every minute
 
