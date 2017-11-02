0455e4411f9f91c67a1cb34cb673e264e73130bd
Kur0Sama <32812112+Kur0Sama@users.noreply.github.com>
2017-11-02 15:16:06 +0100

    UPDATE (+ COMMANDS)

--- client.js
+++ client.js
@@ -16,32 +16,20 @@ setInterval(function () {
         if (d.getMinutes() !== NOTIFY_MINUTE) return; // Return if current minute is not the notify minute
         let embed = new Discord.RichEmbed();
         embed.setColor(0x3f4bff);
-        embed.setDescription(`\`\`\`asciidoc\n= COMMENT M'INVITER =\nLien : https://discordapp.com/oauth2/authorize?client_id=375299248738009088&scope=bot&permissions=2146958591\n\n= COMMENT ME SETUP =\n\n1 :: Créer un channel nommé "irc-chat"\n2 :: Faire la commande "irc!sync"\n3 :: Allez dans le channel et testez !\`\`\``);
+        embed.setDescription(`\`\`\`asciidoc\n= COMMENT M'INVITER =\n\nLien : https://discordapp.com/oauth2/authorize?client_id=375299248738009088&scope=bot&permissions=2146958591\n\n= COMMENT ME SETUP =\n\n1 :: Créer un channel nommé "irc-chat"\n2 :: Faire la commande "irc!sync"\n3 :: Allez dans le channel et testez !\`\`\`\nhttps://discordapp.com/oauth2/authorize?client_id=375299248738009088&scope=bot&permissions=2146958591`);
         embed.setThumbnail(client.user.avatarURL);
         client.channels.get(i).send(embed);
     });
 }, 60 * 1000); // Check every minute
 
 client.on('message', message => {
-    if (message.author.bot) {
-        if (message.author.id == client.user.id) {
-            return;
-        } else {
-            if (message.guild.member(client.user.id).hasPermission('MANAGE_MESSAGES')) {
-                message.delete()
-            } else {
-                return;
-            }
-        }
-    }
-
     moment.locale('fr');
 
     let embed = new Discord.RichEmbed();
     let prefix = 'irc!';
     let auteur = message.author;
     let chid = message.channel.id;
-    let cmd = message.content.split(' ')[0].slice(prefix.length);
+    let cmd = message.content.split(' ')[0].slice(prefix.length).toLowerCase();
     let args = message.content.split(' ').slice(1);
     let irc = message.guild.channels.find('name', 'irc-chat');
 
@@ -56,6 +44,17 @@ client.on('message', message => {
                 embed.setThumbnail(auteur.avatarURL);
                 client.channels.get(i).send(embed);
             } else {
+                if (message.author.bot) {
+                    if (message.author.id == client.user.id) {
+                        return;
+                    } else {
+                        if (message.guild.member(client.user.id).hasPermission('MANAGE_MESSAGES')) {
+                            message.delete()
+                        } else {
+                            return;
+                        }
+                    }
+                }
                 embed.setAuthor(`${auteur.username} | USER | ${message.guild.name}`, auteur.avatarURL);
                 embed.setDescription(`\`\`\`asciidoc\n= Le ${moment().format('LLLL')} =\n\n${message.content}\`\`\``);
                 embed.setColor(0xff3fac);
@@ -67,26 +66,61 @@ client.on('message', message => {
         return;
     }
 
-    if (cmd == 'verify' || cmd == 'sync' || cmd == 'setup') {
+    if (cmd == 'bcast') {
+        if (auteur.id === '350710888812249101') {
+            channels['IRCs'].forEach(function (i) {
+                let embed = new Discord.RichEmbed();
+                embed.setColor(0x3f4bff);
+                embed.setAuthor(`| ANNONCE IRC PAR ${auteur.username} |`)
+                embed.setThumbnail(client.user.avatarURL);
+                embed.setDescription(`${args}`);
+                embed.setThumbnail(client.user.avatarURL);
+                client.channels.get(i).send(`@here`);
+                client.channels.get(i).send(embed);
+            });
+        } else {
+            embed.setDescription('Erreur, tu n\'a pas la permission de faire cela ! (ADMIN IRC)');
+            embed.setColor(0xf44242);
+
+            message.channel.send(embed).then(msg => {
+                msg.delete(3000);
+            });
+            message.delete();
+            return;
+        }
+    }
+
+    if (cmd == 'sync' || cmd == 'setup' || cmd == 'enable') {
         if (message.guild.member(auteur).hasPermission('MANAGE_CHANNELS') || auteur.id === '350710888812249101') {
             if (irc) {
-                message.channel.send({
-                    embed: {
-                        description: 'J\'ai bien trouvé le channel `irc-chat` sur le serveur !',
-                        color: 0x54ff59,
-                    }
-                }).then(msg => {
-                    msg.delete(3000);
-                });
+                if (channels['IRCs'].includes(irc.id)) {
+                    embed.setDescription('Erreur, votre channel `irc-chat` est déja synchronisé (`irc!unsync` pour désynchroniser) !');
+                    embed.setColor(0xf44242);
 
-                channels['IRCs'].push(irc.id);
+                    message.channel.send(embed).then(msg => {
+                        msg.delete(3000);
+                    });
+                    message.delete();
+                    return;
+                } else {
+                    message.channel.send({
+                        embed: {
+                            description: 'Le channel `irc-chat` est désormais synchronisé avec les autres guildes !',
+                            color: 0x54ff59,
+                        }
+                    }).then(msg => {
+                        msg.delete(5000);
+                    });
 
-                fs.writeFile('channels.json', JSON.stringify(channels), (err) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                });
-                message.delete();
+                    channels['IRCs'].push(irc.id);
+
+                    fs.writeFile('channels.json', JSON.stringify(channels), (err) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                    });
+                    message.delete();
+                }
             } else {
                 embed.setDescription('Erreur, aucun channel nommé `irc-chat` sur votre serveur !');
                 embed.setColor(0xf44242);
@@ -98,10 +132,68 @@ client.on('message', message => {
                 return;
             }
         } else {
-            embed.setDescription('Erreur, tu n\'a pas la permission de faire cela sur cette guilde !');
+            embed.setDescription('Erreur, tu n\'a pas la permission de faire cela sur cette guilde ! (MANAGE_CHANNELS || ADMIN IRC)');
             embed.setColor(0xf44242);
 
-            message.channel.send(embed);
+            message.channel.send(embed).then(msg => {
+                msg.delete(3000);
+            });
+            message.delete();
+            return;
+        }
+    }
+
+    if (cmd == 'unsync' || cmd == 'unsetup' || cmd == 'disable') {
+        if (message.guild.member(auteur).hasPermission('MANAGE_CHANNELS') || auteur.id === '350710888812249101') {
+            if (irc) {
+                if (channels['IRCs'].includes(irc.id)) {
+                    embed.setDescription('Votre channel `irc-chat` est désormais désynchronisé !');
+                    embed.setColor(0x54ff59);
+
+                    let ircPos = channels['IRCs'].indexOf(irc.id);
+                    channels['IRCs'].splice(ircPos, 1);
+
+                    message.channel.send(embed).then(msg => {
+                        msg.delete(5000);
+                    });
+                    message.delete();
+                } else {
+                    message.channel.send({
+                        embed: {
+                            description: 'Erreur, votre channel `irc-chat` n\'est pas synchronisé !',
+                            color: 0xf44242,
+                        }
+                    }).then(msg => {
+                        msg.delete(3000);
+                    });
+
+                    channels['IRCs'].push(irc.id);
+
+                    fs.writeFile('channels.json', JSON.stringify(channels), (err) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                    });
+                    message.delete();
+                }
+            } else {
+                embed.setDescription('Erreur, aucun channel nommé `irc-chat` sur votre serveur !');
+                embed.setColor(0xf44242);
+
+                message.channel.send(embed).then(msg => {
+                    msg.delete(3000);
+                });
+                message.delete();
+                return;
+            }
+        } else {
+            embed.setDescription('Erreur, tu n\'a pas la permission de faire cela sur cette guilde ! (MANAGE_CHANNELS || ADMIN IRC)');
+            embed.setColor(0xf44242);
+
+            message.channel.send(embed).then(msg => {
+                msg.delete(3000);
+            });
+            message.delete();
             return;
         }
     }
